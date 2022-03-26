0a53bad03baa70d8db2dc4bc4a6baa18f998a891
nadi <Anderson_rockX@hotmail.com>
2022-03-26 11:32:50 -0300

    Update
    weqw

--- ecommerce/resources/views/categoria/create.blade.php
+++ ecommerce/resources/views/categoria/create.blade.php
@@ -2,11 +2,11 @@
     <x-slot name="header">
         Categorias
     </x-slot>
-
-    <div class="sm:ml-10 p-5 bg-white border-gray-300
-            max-w-7xl mx-auto">
-
-    <form action="{{route('categoria.store')}}" method="post">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                <form action="{{route('categoria.store')}}" method="post">
         @csrf
         <div>
             <x-label>Informe a descricao:</x-label>
@@ -18,7 +18,13 @@
         </div>
     </form>
 
+                </div>
+            </div>
+        </div>
     </div>
+   
+    
+ 
 
 </x-app-layout>
 
