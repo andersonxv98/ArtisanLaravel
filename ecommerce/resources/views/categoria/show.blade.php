a10ad60d4bfc7a8a8132cbb37d0eec7a623975bc
andisu <Anderson_rockX@hotmail.com>
2022-03-29 20:28:31 -0300

    Fazer a model fornecedor
    php artisan make:Model fornecedor --

--- ecommerce/resources/views/categoria/show.blade.php
+++ ecommerce/resources/views/categoria/show.blade.php
@@ -1,5 +1,10 @@
 <?php
     //php artisan keygenerate
-    //php artisan migrate
+    //php artisan migrate 
     //php artisan serve
         //lembrte o wamp deve estar em execução
+
+        //php artisan  make:model fornecedor --migration
+        // no cmd: copy .env.example .env
+                //modificar o arquivo nos paramestro necessário tipo: nomedo db: ecommerce
+        
